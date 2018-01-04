af1dedab12e292b828e741afda6347ae6c9c11b6
SwayGatsby <kayla.fowler@hotmail.com>
2018-01-04 15:10:11 +0000

    added pythonanywhere as allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r1fqe(ouc#z)(5l$993aka#+b1a%(x+s^3hglqk(ysd!hn$1i*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'kaylatab.pythonanywhere.com']
 
 
 # Application definition
